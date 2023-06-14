c2378510ac9d45b6f1cfefbc8b07fb6bec104ba5
Guilherme <guilherme.gh95@gmail.com>
2023-06-14 00:26:43 -0300

    Refactor code and add POST Method

--- demo/demo/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ demo/demo/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,30 +1,36 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
 
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
     @GetMapping(path="list")
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- demo/demo/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ demo/demo/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,21 +1,12 @@
 package academy.devdojo.springboot2.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Anime {
+    public Long id;
     public String name;
 
-    public Anime(String name) {
-        this.name = name;
-    }
-
-    public Anime() {
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
